cebcb6ff8627553d6d9837149904cf994c2b4aec
修仙 <294764484@qq.com>
2023-12-09 18:51:41 +0800

    fix: hand

---
+++ hand-write-everything/1.初级JS/18.Ajax-XMLHttpRequest-Promise-12-9.js
@@ -0,0 +1,48 @@
+// 18.1 XMLHttpRequest
+function ajax(url, successFn) {
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', url, true) // get
+  // xhr.open('POST', url, true) // post
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        // console.log(JSON.parse(xhr.responseText))
+        // alert(xhr.responseText)
+        successFn(xhr.responseText)
+      } else if (xhr.status === 404) {
+        console.log('404 - not found')
+      }
+    }
+  }
+  xhr.send(null) // get
+  const postData = { userName: 'zhangsan', password: 'xxx' }
+  xhr.send(JSON.stringify(postData)) // post
+}
+
+// 18.2 Promise
+function ajax(url) {
+  const p = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET', url, true)
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText))
+        } else if (xhr.status === 404) {
+          reject(new Error('404 - not found'))
+        }
+      }
+    }
+
+    xhr.send(null)
+  })
+  return p
+}
+
+// 测试代码：
+const url = '/api/data.json'
+ajax(url)
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err))
