4ff527c54f604d04326ead15396318586a513994
修仙 <294764484@qq.com>
2024-01-10 09:52:08 +0800

    feat:http

---
+++ hand-write-everything/5.webpack/webpack.base.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { srcPath, distPath }  = require('./paths')
+
+module.exports = {
+  entry: path.join(srcPath, 'index'),
+  module: {
+    
+  }
+}
