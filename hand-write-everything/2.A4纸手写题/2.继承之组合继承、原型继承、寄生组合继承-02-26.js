328ff5785831e343440a2e7713e13e3652604366
修仙 <294764484@qq.com>
2024-02-26 19:46:21 +0800

    feat: 复习：3.双飞翼布局/5.flex三色骰子/2.继承-画图理解

--- hand-write-everything/2.A4纸手写题/2.继承之组合继承、原型继承、寄生组合继承-02-26.js
+++ hand-write-everything/2.A4纸手写题/2.继承之组合继承、原型继承、寄生组合继承-02-26.js
@@ -10,14 +10,12 @@ function Child() {}
 Child.prototype = new Parent()
 
 var child1 = new Child()
-console.log(child1.getName())
-// child1.name = 'child1'                x
-// var child2 = new Child()              x
-// console.log(child2.name)              x
+child1.name = 'child1'
+child1.getName()
 
-// 问题：引用类型属性，被所有实例共享。举个例子：
+// 问题：引用类型的属性，所有实例共享；
 function Parent() {
-  this.names = ['red', 'yellow', 'blue']
+  this.names = ['1', '2', '3']
 }
 
 function Child() {}
@@ -31,7 +29,7 @@ console.log(child2.names)
 
 // 9.2 借用构造函数
 function Parent() {
-  this.names = ['red', 'yellow', 'blue']
+  this.names = ['1', '2', '3']
 }
 
 function Child() {
@@ -44,24 +42,31 @@ console.log(child1.names)
 var child2 = new Child()
 console.log(child2.names)
 
-// 优点1：避免了引用类型属性，被所有实例共享。
+// 优点1：避免了引用类型属性，被所有实例共享的问题。
 
 // 优点2：Child 可以向 Parent 传参。
 
 function Parent(name) {
   this.name = name
+  this.colors = ['red', 'yellow', 'blue']
 }
 
-function Child(name) {
+function Child(name, age) {
   Parent.call(this, name)
+  this.age = age
 }
 
-var child1 = new Child('child1')
+var child1 = new Child('child1', 18)
+child1.colors.push('test')
 console.log(child1.name)
-var child2 = new Child('child2')
+console.log(child1.age)
+console.log(child1.colors)
+var child2 = new Child('child2', 20)
 console.log(child2.name)
+console.log(child2.age)
+console.log(child2.colors)
 
-// 缺点：方法都在构造函数中定义，每次创建实例，都会创建一遍方法；
+// !缺点：【忘记】：方法都在构造函数中定义，每次创建实例，都会创建一遍方法；
 
 // 9.3 组合继承
 function Parent(name) {
@@ -80,7 +85,7 @@ Child.prototype = new Parent()
 Child.prototype.constructor = Child
 
 var child1 = new Child('child1', 18)
-child1.colors.push('green')
+child1.colors.push('test')
 console.log(child1.name)
 console.log(child1.age)
 console.log(child1.colors)
@@ -114,13 +119,27 @@ person1.colors.push('test')
 console.log(person2.name)
 console.log(person2.colors)
 
-// 缺点：和原型链继承一样，引用类型的属性，所有实例都共享。
+// 缺点：同原型链继承一样，引用类型的属性，所有实例都共享。
 
 // 9.5 寄生式继承
 function createObj(o) {
-  var clone = Object.create(o) // ②该函数在内部以某种形式来做增强对象，
+  var clone = Object.create(o) // ! 2.该函数在内部以某种形式来做增强对象，
   clone.sayName = function () {
     console.log('hi')
   }
-  return clone // ③最后返回对象。
-} // ①创建一个仅用于封装继承过程的函数。
+  return clone // ! 3.最后返回对象。
+} // ! 1.创建一个仅用于封装继承过程的函数。
+
+const person_ = {
+  name: 'person_',
+  colors: ['red', 'yellow', 'blue']
+}
+
+var person_1 = createObj(person_)
+person_1.name = 'person_1'
+person_1.colors.push('test')
+var person_2 = createObj(person_)
+console.log(person_2.name)
+console.log(person_2.colors)
+
+person_1.sayName() // hi
