48724a0870937dfca397b730a02ef346b22a5fab
修仙 <294764484@qq.com>
2024-02-29 14:11:31 +0800

    feat: 15.手写 DOM 节点操作(4)、结构操作(4)、性能优化(2)/16.手写 BOM（navigator、screen、location、history）

--- hand-write-everything/1.初级JS/15.DOM-02-29.js
+++ hand-write-everything/1.初级JS/15.DOM-02-29.js
@@ -12,30 +12,27 @@ const containerList = document.getElementsByClassName('container')
 const pList = document.querySelectorAll('p')
 
 // 2.attribute 方式
-
 const p1 = pList[0]
 // 设置/修改属性
-p1.setAttribute('data-name', 'imooc')
-p1.setAttribute('style', 'font-size: 50px;')
+p1.setAttribute('data-name', 'xxx')
+p1.setAttribute('style', 'font-size: 20px;')
 // 获取属性
 p1.getAttribute('data-name')
 p1.getAttribute('style')
 
-// 3.property 方式（以页面渲染结构的形式，修改js属性改变页面样式）
-
+// 3.property 方式
 // 设置/修改属性
 p1.style.width = '100px'
-p1.className = 'red'
+p1.className = 'p'
 // 获取属性
 p1.style.width
 p1.className
 p1.nodeName
 p1.nodeType
 
-// 4.attr VS property 区别
-
-// ①修改html属性，改变html结构
-// ②修改对象属性，不会体现到html结构
+// 4.attr VS property
+// ①修改html，改变html结构
+// ②修改对象属性，不改变html结构
 
 /**
  * -------------------------------
@@ -46,41 +43,38 @@ p1.nodeType
 const p1 = document.createElement('p')
 p1.innerHTML('p1')
 body.appendChild(p1)
-const p2 = document.getElementById('p')
+const p2 = document.getElementById('p2')
 body.appendChild(p2)
 // 2.获取子列表/父元素
 const child = div1.childNodes
 const parent = div1.parentNode
 // 3.删除子元素
-div1.removeChild(div1.childNodes[0])
-// 4.子元素列表过滤节点类型为1的元素节点
-const div1ChildNodesP = Array.prototype.slice
-  .call(div1.childNodes)
-  .filter((child) => {
-    if (child.nodeType === 1) {
-      return true
-    }
-    return false
-  })
-console.log(div1ChildNodesP) // div1ChildNodesP (3) [p, p, p]
+divList.removeChild(divList.childNodes[0])
+// 4.子元素列表过滤子元素节点类型为1的节点
+const list = Array.prototype.slice.call(divList.childNodes).filter((child) => {
+  if (child.nodeType === 1) {
+    return true
+  }
+  return false
+})
+console.log(list)
 
 /**
  * -------------------------------
  * 三、DOM性能优化
  */
 
-// 1.DOM操作/DOM查询“昂贵”，避免频繁操作：
-//   (耗费性能，占用CPU多-> 浏览器重绘重排-> 频繁操作 -> 卡顿)
+// 1.DOM操作/DOM查询“昂贵”，避免频繁操作。
 for (let i = 0; i < document.getElementsByTagName('p').length; i++) {
   // ...
 }
-// 2.DOM查询做缓存（查、存，不再查）（变量存length，避免频繁dom操作）（一定记住，几率大）
+// 2.DOM查询做缓存（变量存length，避免频繁dom操作）（一定记住，几率大）
 const pList1 = document.getElementsByTagName('p')
 const length = pList1.length
 for (let i = 0; i < length; i++) {
   // ...
 }
-// 3.将频繁操作改为一次性操作（临时区域，js变量操作，较快）
+// 3.将频繁操作改为一次性操作（临时区域，js变量存length操作，较快）
 const listNode = document.getElementById('list')
 const frag = document.createDocumentFragment()
 for (let x = 0; x < 10; x++) {
