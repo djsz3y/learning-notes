9005ae1821e1b98be6d146d10ec58bdc441ed338
修仙 <294764484@qq.com>
2024-03-30 20:53:48 +0800

    feat: 1.初级JS：2~8

---
+++ hand-write-everything/1.初级JS/7.deepClone-03-30.js
@@ -0,0 +1,21 @@
+function deepClone(obj = {}) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj
+  }
+
+  let result
+  if (obj instanceof Array) {
+    // Array.isArray(obj)
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key])
+    }
+  }
+
+  return result
+}
